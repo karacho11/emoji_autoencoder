e5a4db8d2d82fcb869e1c3c5576db1f3ab3ea3b3
Kara Cho <karacho@MacBook-Air-747.local>
2025-11-26 22:13:46 +0900

    dataset initial

---
+++ src/dataset.py
@@ -0,0 +1,4 @@
+from datasets import load_dataset
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from PIL import Image
--- src/train.py
+++ src/train.py
@@ -1,6 +1,3 @@
-from datasets import load_dataset
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms
-from PIL import Image
+
 
 
